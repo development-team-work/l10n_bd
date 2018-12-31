02e9d404c3d751eb93bf584dfe7a2f10d6c28704
Khanstore <dmashraf@gmail.com>
2018-12-31 15:50:38 +0600

    update __init__py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Copyright (C) 2011 Smartmode LTD (<http://www.smartmode.co.uk>).
+# Copyright (C) 2018 Eagle ERP Ltd (<http://www.eagle_erp.com>).
